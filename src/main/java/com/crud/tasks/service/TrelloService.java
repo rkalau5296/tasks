18c888b07142768619a146c1ec66a32f92aefa54
rkalau <rafal.kalata.itservice@gmail.com>
2020-01-09 00:04:09 +0100

    23.4 start working

---
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -0,0 +1,33 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.CreatedTrelloCard;
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TrelloService {
+    @Autowired
+    private TrelloClient trelloClient;
+    @Autowired
+    private SimpleEmailService emailService;
+    @Autowired
+    private AdminConfig adminConfig;
+
+    private static final String SUBJECT = "Tasks: New Trello card";
+
+    public List<TrelloBoardDto> fetchTrelloBoards() {
+        return trelloClient.getTrelloBoards();
+    }
+    public CreatedTrelloCard createdTrelloCard(final TrelloCardDto trelloCardDto) {
+        CreatedTrelloCard newCard = trelloClient.createNewCard(trelloCardDto);
+        emailService.send(new Mail(adminConfig.getAdminMail(), SUBJECT, "New card: "+ trelloCardDto.getName() + " has been created on your Trello account", ""));
+        return newCard;
+    }
+}
