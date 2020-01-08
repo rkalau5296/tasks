94f90b953fa7185e6807d0eaef5324eb7442bd96
rkalau <rafal.kalata.itservice@gmail.com>
2020-01-08 23:20:55 +0100

    23.3 cd

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.domain.Mail;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
@@ -7,26 +8,30 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Service
 public class SimpleEmailService {
-    @Autowired
-    private JavaMailSender javaMailSender;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SimpleMailMessage.class);
 
-    public void send(final String receiverEmail, final String subject, final String message){
+    @Autowired
+    private JavaMailSender javaMailSender;
 
+    public void send(final Mail mail){
+        LOGGER.info("Starting email preparation...");
         try {
-            SimpleMailMessage mailMessage = new SimpleMailMessage();
-            mailMessage.setTo(receiverEmail);
-            mailMessage.setSubject(subject);
-            mailMessage.setText(message);
-
-            javaMailSender.send(mailMessage);
-        }catch (MailException e){
+            javaMailSender.send(createMailMessage(mail));
+            LOGGER.info("Email has been sent.");
+        } catch (MailException e) {
             LOGGER.error("Failed to process email sending: ", e.getMessage(), e);
         }
+    }
+
+    private SimpleMailMessage createMailMessage (final Mail mail) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+        return mailMessage;
     }
 }
