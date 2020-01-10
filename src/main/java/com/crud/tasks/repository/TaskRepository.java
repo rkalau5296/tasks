42804dd9549cf24863c819782518f1bcdd8b8524
rkalau <rafal.kalata.itservice@gmail.com>
2020-01-11 00:31:53 +0100

    23.4 finished working

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 public interface TaskRepository extends CrudRepository<Task, Long>{
     @Override
     List<Task> findAll();
+
     @Override
     Optional<Task> findById(Long id);
 
@@ -18,4 +19,6 @@ public interface TaskRepository extends CrudRepository<Task, Long>{
     @Override
     void deleteById(Long id);
 
+    @Override
+    long count();
 }
---
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -0,0 +1,40 @@
+package com.crud.tasks.scheduler;
+
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.SimpleEmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailScheduler {
+
+    @Autowired
+    private SimpleEmailService simpleEmailService;
+    @Autowired
+    private TaskRepository taskRepository;
+    @Autowired
+    private AdminConfig adminConfig;
+
+    private static final String SUBJECT = "Tasks: Once a day email";
+
+    @Scheduled(fixedDelay = 10000)
+    public void sendInformationEmail(){
+        long size = taskRepository.count();
+        simpleEmailService.send(new Mail(
+                adminConfig.getAdminMail(),
+                SUBJECT,
+                setText(size),
+                "test@test.pl")
+        );
+    }
+
+    private String setText(long size){
+        if (size == 1)
+            return "Currently in database you got: " + size + " task";
+        else
+            return "Currently in database you got: " + size + " tasks";
+    }
+}
