2c64b45aa0afcfd936862d437b5f636450af6ed9
rkalau <rafal.kalata.itservice@gmail.com>
2020-01-26 00:35:11 +0100

    32.3 start working

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
     @Autowired
@@ -15,15 +18,24 @@ public class MailCreatorService {
 
     @Autowired
     private AdminConfig adminConfig;
+
     public String buildTrelloCardEmail(String message) {
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides conection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://localhost:8888/crud");
         context.setVariable("button", "Visit website");
-        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("admin_name", adminConfig);
         context.setVariable("goodbye_message", "Hope we'll meet you again. See you next time.");
         context.setVariable("company_details", "Adress: 1 Test street 1 12345 TestCity.");
         context.setVariable("preview_message", "This is some preview message.");
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", false);
+        context.setVariable("application_functionality", functionality);
         return templateEngine.process("mail/create-trello-card-mail", context);
     }
 }
