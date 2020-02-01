481beb5671c906e119755a3798570ab8489333ec
rkalau <rafal.kalata.itservice@gmail.com>
2020-02-01 12:31:52 +0100

    fix two failure tests

--- src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -6,8 +6,12 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.mail.MailMessage;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -35,7 +39,7 @@ public class SimpleEmailServiceTest {
         simpleEmailService.send(mail);
 
         // Then
-        verify(javaMailSender, times(1)).send(mailMessage);
+        verify(javaMailSender, times(1)).send(any(MimeMessagePreparator.class));
     }
     @Test
     public void shouldSendEmailWithEmptyCC(){
@@ -51,6 +55,7 @@ public class SimpleEmailServiceTest {
         simpleEmailService.send(mail);
 
         // Then
-        verify(javaMailSender, times(1)).send(mailMessage);
+        verify(javaMailSender, times(1)).send(any(MimeMessagePreparator.class));
+
     }
 }
