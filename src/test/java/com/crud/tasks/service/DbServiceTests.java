eb794cbf217caea04f592b4a9b2e800cc855bc03
rkalau <rafal.kalata.itservice@gmail.com>
2020-02-02 01:55:03 +0100

    29.2 class 100% methods 08%  AT LAST!!! FINALLY!!!

---
+++ src/test/java/com/crud/tasks/service/DbServiceTests.java
@@ -0,0 +1,41 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.repository.TaskRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DbServiceTests {
+
+    @InjectMocks
+    private DbService dbService;
+    @Mock
+    private TaskRepository repository;
+    @Test
+    public void getAllTasksTest() {
+        //Given
+        List<Task> tasks = new ArrayList<>();
+        tasks.add(new Task(1L, "title", "content"));
+
+        //When
+        when(repository.findAll()).thenReturn(tasks);
+
+        //Then
+        List<Task> taskList = dbService.getAllTasks();
+
+        assertEquals(tasks.get(0).getId(), taskList.get(0).getId());
+        assertEquals(tasks.get(0).getTitle(), taskList.get(0).getTitle());
+        assertEquals(tasks.get(0).getContent(), taskList.get(0).getContent());
+    }
+}
